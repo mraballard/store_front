9bb39ed58157417cd15914ce4d9ce767722be9a5
Aaron Ballard <mraballard@gmail.com>
2016-10-17 18:02:00 -0300

    added code for MainRouter in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,45 @@
 
   function MainRouter($stateProvider, $urlRouterProvider, $locationProvider){
 
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/index");
 
     $stateProvider
-    .state('index'), {
+    .state('index', {
       url: '/index',
       params: {
         user: null
       }
-    }
+    })
+    .state('login',{
+      url: '/login',
+      templateUrl: 'login.html',
+      controller: 'AuthController',
+      controllerAs: 'authCtrl'
+    })
+    .state('signup',{
+      url: '/signup',
+      templateUrl: 'singup.html',
+      controller: 'AuthController',
+      controllerAs: 'authCtrl'
+    })
+    .state('home',{
+      url: '/home',
+      templateUrl: 'home.html',
+      controller: 'StoreController',
+      controllerAs: 'storeCtrl'
+    })
+    .state('cart',{
+      url: '/cart',
+      templateUrl: 'cart.html',
+      controller: 'StoreController',
+      controllerAs: 'storeCtrl'
+    })
+    .state('orders',{
+      url: '/orders',
+      templateUrl: 'orders.html',
+      controller: 'StoreController',
+      controllerAs: 'storeCtrl'
+    })
 
     $locationProvider.html5Mode({
       enabled: true,
