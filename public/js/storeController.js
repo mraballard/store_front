30bd77d2090dbb618ac11d36d52ff8bd0b3ac32a
Aaron Ballard <mraballard@gmail.com>
2016-10-18 16:03:53 -0300

    added methods to storeController and  service

--- public/js/storeController.js
+++ public/js/storeController.js
@@ -9,10 +9,12 @@
     this.all = [];
     var self = this;
     this.searchStr = ''; // Initialize search value to '' to return all products in database
+    this.quantityAtShopIndex = {};  // used for reseting quantity in dropdown menu of product page
+    this.quantityAtCartIndex = {};  // used for reseting quantity in dropdown menu of cart page
 
     // GET PRODUCTS FROM DATABASE
     this.getProducts = function(searchStr) {
-      $http.get('')
+      $http.get('/api/products/')
       .catch(function(error){
         console.log(error);
       })
@@ -24,9 +26,27 @@
       .catch(function(error){
         console.log(error);
       });
-    }
+    };
+    this.addToCart = function(id,quantity) {
+      if (!quantity) {
+        $state.go('home', {url: '/home'})
+        .catch(function(error){
+          console.log(error);
+        });
+      } else {
+        this.cartHasItems = true;
+        $cart.add(id,quantity);
+        this.quantityAtShopIndex[index] = 0;
+      }
+    };
+    this.updateCart = function(newQuantity, index){
+      $cart[index].quantity = newQuantity;
+      self.quantityAtCartIndex[index] = 0;
+    };
+    this.deleteFromCart = function(index) {
+      $cart.items.splice(index,1);
+    };
 
-    console.log('this is the cart: '+$cart);
   }
 
 })()
