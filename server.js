3a9002398292b1b105d77f21cbce61e9d61c1ad4
Aaron Ballard <mraballard@gmail.com>
2016-10-20 11:34:36 -0300

    small change to server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var logger          = require('morgan');
 var methodOverride  = require('method-override');
 var passport        = require('passport');
 var LocalStrategy   = require('passport-local').Strategy;
-var port            = 4000 || process.env.PORT;
+var port            = process.env.PORT || 4000;
 var app             = express();
 
 mongoose.Promise = global.Promise;
