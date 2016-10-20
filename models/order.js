85c855f379c0dd3c175033a53000b746fdca195b
Aaron Ballard <mraballard@gmail.com>
2016-10-20 12:24:08 -0300

    changed 'require('./orderItem' to 'orderitem'

--- models/order.js
+++ models/order.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var OrderItemSchema = require('./orderItem').schema;
+var OrderItemSchema = require('./orderitem').schema;
 var UserSchema = require('./user').schema;
 
 OrderItemSchema.virtual('subtotal').get(function(){
